b8901f20551addf6ea9cf42f2a21ac1980191963
Nebulaevo <didier.martin.dev@gmail.com>
2025-10-18 13:02:22 +0200

    finished extended url mixin tests

--- test/extended-url-mixin.test.ts
+++ test/extended-url-mixin.test.ts
@@ -1,52 +1,104 @@
 import { describe, expect, it } from 'vitest'
 
-import XUrl from '../src/classes/xurl'
-import RelativeUrl from '../src/classes/relative-url'
+import UrlRepr from '../src/classes/url-repr'
 
 import { getFreshInstances } from './helpers/fresh-instances'
 
 
+
 describe("#ExtendedUrlMixin", () => {
-    it('can set pathname from string array', () => {
+    it('"setPathnameSegments" : works with valid argument', () => {
 
         const testValues = [
-            {
-                array: [],
+            {   arg: [],
                 result: '/'
             },
-            {
-                array: ['a', 'b', 'c'],
+            {   arg: ['a', 'b', 'c'],
                 result: '/a/b/c/'
             },
-            {
-                array: ['my', 'PATH'],
-                result: '/my/PATH0/'
+            {   arg: ['my', 'PATH'],
+                result: '/my/PATH/'
+            },
+            {   arg: ['шеллы'],
+                result: '/%D1%88%D0%B5%D0%BB%D0%BB%D1%8B/'
+            },
+            {   arg: [';,/?:@&=+$"`# '],
+                result: '/%3B%2C%2F%3F%3A%40%26%3D%2B%24%22%60%23%20/'
+            },
+            {   arg: ["-_.!~*'()"],
+                result: "/-_.!~*'()/"
             },
         ]
         
 
-        for (const testValue of testValues) {            
+        for (const {arg, result} of testValues) {            
             const freshInstances = getFreshInstances(
                 'https://user:password@sub.domain.com:8000/test/?query=fish&filter=yellow'
             )
-            // if instance creation failed it's not the responsability of this test
+            // if instance creation failed 
+            // it's not the responsability of this test
             if (!freshInstances) return
             
-            const {
-                url,
-                relativeUrl
-            } = freshInstances
+            const { url, relativeUrl } = freshInstances
 
-            console.log(url)
-            console.log(relativeUrl)
+            url.setPathnameSegments(arg)
+            relativeUrl.setPathnameSegments(arg)
 
-            url.setPathnameSegments(testValue.array)
-            relativeUrl.setPathnameSegments(testValue.array)
-
-            expect(url.pathname).toBe(testValue.result)
-            expect(relativeUrl.pathname).toBe(testValue.result)
+            expect(url.pathname).toBe(result)
+            expect(relativeUrl.pathname).toBe(result)
         }
-
     })
 
+    it('"setPathnameSegments" : fails with invalid argument', () => {
+
+        const testValues: any[] = [
+            undefined,
+            null,
+            0, 1,
+            'abc',
+            new Set(['a', 'b', 'c']),
+            [1,2,3],
+            { a: 'a', b: 'b'}
+        ]
+        
+        for (const invalidArg of testValues) {            
+            const freshInstances = getFreshInstances(
+                'https://user:password@sub.domain.com:8000/test/?query=fish&filter=yellow'
+            )
+            // if instance creation failed 
+            // it's not the responsability of this test
+            if (!freshInstances) return
+            
+            const { url, relativeUrl } = freshInstances
+            
+            expect(() => url.setPathnameSegments(invalidArg)).toThrowError(TypeError)
+            expect(url.pathname).toBe('/test/')
+
+            expect(() => relativeUrl.setPathnameSegments(invalidArg)).toThrowError(TypeError)
+            expect(relativeUrl.pathname).toBe('/test/')
+        }
+    })
+
+
+    it('"as" : returns an instance of UrlRepr and caches it', () => {
+
+        const freshInstances = getFreshInstances(
+            'https://user:password@sub.domain.com:8000/test/?query=fish&filter=yellow'
+        )
+        // if instance creation failed 
+            // it's not the responsability of this test
+        if (!freshInstances) return
+        
+        const { url, relativeUrl } = freshInstances
+
+        const urlRepr = url.as
+        const relativeUrlRepr = relativeUrl.as
+
+        expect(urlRepr).toBeInstanceOf(UrlRepr)
+        expect(relativeUrlRepr).toBeInstanceOf(UrlRepr)
+
+        // Double checking that the instance is cached
+        expect(urlRepr).toBe(url.as)
+        expect(relativeUrlRepr).toBe(relativeUrl.as)
+    })
 })
