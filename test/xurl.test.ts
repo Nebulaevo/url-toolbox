1c470b8ca8abe5812935c154133a0744ba4406ec
Nebulaevo <didier.martin.dev@gmail.com>
2025-10-22 02:39:43 +0200

    re-organised XUrl tests

--- test/xurl.test.ts
+++ test/xurl.test.ts
@@ -2,104 +2,227 @@ import { describe, expect, it } from 'vitest'
 
 import XUrl from '../src/classes/xurl'
 import { BrokenUrlRestrictionError } from '../src/classes/errors'
+import RelativeUrl from '../src/classes/relative-url'
+
+import { instancesAreEquivalent } from './helpers/instance-comparaison'
+import { getUrlTransformations } from './helpers/get-url-transformations'
 
 describe("#XUrl", () => {
-    it('can create instances with valid url and restrictions', () => {
+    
+    it('"instance initialisation" : works with valid values', () => {
+        // (also checks if credentials restriction successfully filters credentials at creation time)
+        const testUrlData = {
+            url: new URL('https://user:password@sub.domain.com:8000/test/?query=fish&filter=yellow'),
+            urlNoCreds: new URL('https://sub.domain.com:8000/test/?query=fish&filter=yellow'),
+            
+            xUrl: new XUrl('https://user:password@sub.domain.com:8000/test/?query=fish&filter=yellow'),
+            relativeUrl: new RelativeUrl('/test/?query=fish&filter=yellow'),
 
-        const url = {
             full: 'https://user:password@sub.domain.com:8000/test/?query=fish&filter=yellow',
-            fullNoCreds: 'https://sub.domain.com:8000/test/?query=fish&filter=yellow',
             base: 'https://user:password@sub.domain.com:8000',
             tail: '/test/?query=fish&filter=yellow'
         }
 
-        const restrictions = {
-            A: {allowedProtocols: ['http:', 'https:']},
-            B: {allowedHosts: ['sub.domain.com:8000', 'test.com']},
-            C: {ignoreCredentials: true},
-            D: {
+        const restrictions: (Partial<XUrl.UrlRestrictions_T>|undefined)[] = [
+            undefined,
+            {allowedProtocols: ['http:', 'https:']},
+            {allowedProtocols: ['http', 'https']}, // with forgotten ':'
+            {allowedHosts: ['sub.domain.com:8000', 'test.com']},
+            {ignoreCredentials: true},
+            {
                 allowedProtocols: ['http:', 'https:'], 
                 allowedHosts: ['sub.domain.com:8000'],
                 ignoreCredentials: true
             }
-        }
+        ]
 
-        const constructorArgs = [
-            [url.full, undefined, undefined],
-            [url.tail, url.base, undefined],
-
-            [url.full, undefined, restrictions.A],
-            [url.tail, url.base, restrictions.A],
-
-            [url.full, undefined, restrictions.B],
-            [url.tail, url.base, restrictions.B],
-
-            [url.full, undefined, restrictions.C],
-            [url.tail, url.base, restrictions.C],
-
-            [url.full, undefined, restrictions.D],
-            [url.tail, url.base, restrictions.D],
+        const constructorArgs: [string|URL, string|URL|undefined][] = [
+            [testUrlData.full, undefined],
+            [testUrlData.url, undefined],
+            [testUrlData.xUrl, undefined],
+            [testUrlData.tail, testUrlData.base],
+            [testUrlData.tail, testUrlData.url],
+            [testUrlData.tail, testUrlData.xUrl],
+            [testUrlData.relativeUrl, testUrlData.base],
+            [testUrlData.relativeUrl, testUrlData.url],
+            [testUrlData.relativeUrl, testUrlData.xUrl],
         ] as const
 
-        for (const args of constructorArgs) {
-            const restrictions = args[2] as Partial<XUrl.UrlRestrictions_T> | undefined
-            const xUrl = new XUrl(...args as [string, string|undefined, Partial<XUrl.UrlRestrictions_T>|undefined])
-            
-            if (restrictions?.ignoreCredentials) {
-                expect(xUrl.href).toBe(url.fullNoCreds)
-            } else {
-                expect(xUrl.href).toBe(url.full)
+        for (const restriction of restrictions) {
+            for (const args of constructorArgs) {
+                const xUrlA = new XUrl(...args, restriction)
+                const xUrlB = XUrl.parse(...args, restriction)
+                const referenceUrl = restriction?.ignoreCredentials
+                    ? testUrlData.urlNoCreds
+                    : testUrlData.url
+                
+                expect(XUrl.canParse(...args)).toBe(true)
+                expect(instancesAreEquivalent(referenceUrl, xUrlA)).toBe(true)
+                expect(xUrlB).not.toBeNull()
+                expect(instancesAreEquivalent(referenceUrl, xUrlB!)).toBe(true)
             }
         }
     }) 
 
-    it('prevents dangerous and unknown protocols by default', () => {
+    it('"instance initialisation" : fails with TypeError for invalid urls', () => {
 
-        const dangerousProtocols = ['javascript:', 'data:', 'vbscript:', 'unknown-protocol:']
+        const invalidConstructorArgs: [any, any][] = [
+            // invalid urls with no base
+            ['justastring', undefined],
+            ['http://in valid.com', undefined],
+            ['://missing-protocol.com', undefined],
+            ['http//missing-colon.com', undefined],
+            ['http://', undefined],
+            ['', undefined],
+            [null, undefined],
+            [undefined, undefined],
+            [12345, undefined],
+            [{}, undefined],
+            [[], undefined],
 
-        const dangerousUrls = [
-            'javascript:alert("XSS")',
-            'data:text/html;base64,PHNjcmlwdD5hbGVydCgiWFNTIik8L3NjcmlwdD4=',
-            'vbscript:msgbox("XSS")',
-            'unknown-protocol://some/resource'
+            // invalid bases
+            ['/', 'justastring'],
+            ['/', 'http://in valid.com'],
+            ['/', '://missing-protocol.com'],
+            ['/', 'http//missing-colon.com'],
+            ['/', 'http://'],
+            ['/', ''],
+            ['/', 12345],
+            ['/', {}],
+            ['/', []],
         ]
 
-        for (const dangerousUrl of dangerousUrls) {
-            const url = new XUrl('https://example.com')
-            
-            expect(() => new XUrl(dangerousUrl)).toThrow(BrokenUrlRestrictionError)
-            expect(() => url.href = dangerousUrl).toThrow(BrokenUrlRestrictionError)
-        }
+        for (const invalidArgs of invalidConstructorArgs) {
+            expect(() => new XUrl(...invalidArgs)).toThrowError(TypeError)
+            expect(XUrl.parse(...invalidArgs)).toBe(null)
+            expect(XUrl.canParse(...invalidArgs)).toBe(false)
 
-        for (const dangerousProtocol of dangerousProtocols) {
-            // have to start from a base-less protocol
-            // so that URL allows setting javascrip:, vbscript: and data: protocols
-            const url = new XUrl('mailto:me@you.they') // 
-            expect(() => url.protocol = dangerousProtocol).toThrow(BrokenUrlRestrictionError)
-        }
-    }) 
-
-    it('"protocol and host restrictions" are enforced at creation time', () => {
-
-        const restrictions = {
-            allowedProtocols: ['https:'],
-            allowedHosts: ['allowed.com']
-        }
-        const invalidUrls = [
-            'http://allowed.com/resource', // invalid protocol
-            'https://forbidden.com/resource', // invalid host
-            'https://forbidden.allowed.com/resource', // invalid host
-            'https://allowed.com:8000/resource', // invalid host
-            'ftp://forbidden.com/resource' // invalid protocol and host
-        ]
-
-        for (const invalidUrl of invalidUrls) {
-            expect(() => new XUrl(invalidUrl, undefined, restrictions))
-                .toThrow(BrokenUrlRestrictionError)
+            // sanity checks verifying that URL behaves the same way
+            expect(() => new URL(...invalidArgs)).toThrowError(TypeError)
+            expect(URL.parse(...invalidArgs)).toBe(null)
+            expect(URL.canParse(...invalidArgs)).toBe(false)
         }
     })
 
-    it('"protocol and host restrictions" are enforced at modifcation time', () => {
+    it('"instance initialisation" : fails with BrokenUrlRestrictionError if restrictions are broken', () => {
+
+        const testData: {
+            restrictions: Partial<XUrl.UrlRestrictions_T>|undefined,
+            invalidArgs: [any, any][]
+        }[] = [
+            // check that default restrictions are applied
+            {   restrictions: undefined, 
+                invalidArgs: [
+                    ['javascript:alert("XSS")', undefined],
+                    ['data:text/html;base64,PHNjcmlwdD5hbGVydCgiWFNTIik8L3NjcmlwdD4=', undefined],
+                    ['vbscript:msgbox("XSS")', undefined],
+                    ['unknown-protocol://some/resource', undefined]
+                ],
+            },
+            // protocol restrictions
+            {   restrictions: {
+                    allowedProtocols: ['http:', 'https:']
+                },
+                invalidArgs: [
+                    ['ftp://domain.com/path/', undefined],
+                    ['mailto:me@you.us', undefined],
+                    ['/', 'ftp://domain.com'],
+                ],
+            },
+            // protocol restrictions
+            // (without ':' at the end of the protocols)
+            {   restrictions: { 
+                    allowedProtocols: ['http', 'https']
+                },
+                invalidArgs: [
+                    ['ftp://domain.com/path/', undefined],
+                    ['mailto:me@you.us', undefined],
+                    ['/', 'ftp://domain.com'],
+                ],
+            },
+            // host restrictions
+            {   restrictions: {
+                    allowedHosts: ['allowed.com']
+                },
+                invalidArgs: [
+                    // invalid domain
+                    ['http://forbidden.com/path/', undefined],
+                    ['/', 'http://forbidden.com'],
+                    // invalid sub-domain
+                    ['http://forbidden.allowed.com/path/', undefined],
+                    ['/', 'http://forbidden.allowed.com'],
+                    // invalid port
+                    ['http://allowed.com:8080/path/', undefined],
+                    ['/', 'http://allowed.com:8080'],
+                    // non classic protocols
+                    // (empty host)
+                    ['mailto:me@you.us', undefined],
+                    ['tel:+1234567890', undefined],
+                ],
+            },
+            // restricted protocols and hosts
+            {   restrictions: {
+                    allowedProtocols: ['http:', 'https:'],
+                    allowedHosts: ['allowed.com']
+                },
+                invalidArgs: [
+                    // invalid protocol
+                    ['ftp://allowed.com/path/', undefined],
+                    ['/', 'ftp://allowed.com'],
+                    // invalid domain
+                    ['http://forbidden.com/path/', undefined],
+                    ['/', 'http://forbidden.com'],
+                    // invalid sub-domain
+                    ['http://forbidden.allowed.com/path/', undefined],
+                    ['/', 'http://forbidden.allowed.com'],
+                    // invalid port
+                    ['http://allowed.com:8080/path/', undefined],
+                    ['/', 'http://allowed.com:8080'],
+                    // invalid protocol and host
+                    ['ftp://forbidden.com/path/', undefined],
+                    ['/', 'ftp://forbidden.com'],
+                    // non classic protocols
+                    // (empty host)
+                    ['mailto:me@you.us', undefined],
+                    ['tel:+1234567890', undefined],
+                ],
+            },
+        ]
+
+        for (const {restrictions, invalidArgs} of testData) {
+            for (const args of invalidArgs) {
+                expect(() => new XUrl(...args, restrictions)).toThrowError(BrokenUrlRestrictionError)
+                expect(XUrl.parse(...args, restrictions)).toBe(null)
+                expect(XUrl.canParse(...args, restrictions)).toBe(false)
+            }
+        }
+    }) 
+
+    it('"operations" : behaviour is coherent with the native URL class', () => {
+        
+        const urlStrings = [
+            'https://bob:password123@some.where.gg:8008/sea/?query=fish',
+            'ftp://bob:password123@some.where.gg:8008/sea/?query=fish',
+            'mailto:you@yourhouse.mail',
+            'tel:+1234567890',
+        ]
+
+        const transformations = getUrlTransformations('ABSOLUTE')
+
+        for (const urlString of urlStrings) {
+
+            const url = new URL(urlString)
+            const xUrl = new XUrl(urlString)
+            
+            for (const transform of transformations) {
+                transform(url)
+                transform(xUrl)
+                expect(instancesAreEquivalent(url, xUrl)).toBe(true)
+            }
+        }
+    })
+
+    it('"protocol and host restrictions" : are enforced at modifcation time', () => {
 
         const restrictions = {
             allowedProtocols: ['https:'],
@@ -134,25 +257,7 @@ describe("#XUrl", () => {
         }
     })
 
-    it('"ignoreCredentials restriction" filters out credentials at creation time', () => {
-
-        const restrictions = {
-            ignoreCredentials: true
-        }
-
-        const urls = [
-            'http://user:password@domain.com/resource',
-            'http://user@domain.com/resource',
-            'http://:password@domain.com/resource',
-        ]
-        
-        for (const urlString of urls) {
-            const xUrl = new XUrl(urlString, undefined, restrictions)
-            expect(xUrl.href).toBe('http://domain.com/resource')
-        }
-    })
-
-    it('"ignoreCredentials restriction" filters out credentials at modification time', () => {
+    it('"credentials restriction" : filters out credentials at modification time', () => {
 
         const restrictions = {
             ignoreCredentials: true
