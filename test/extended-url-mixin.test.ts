6431afc3004e5ad45773f2ebd64ea912fc857f8a
Nebulaevo <didier.martin.dev@gmail.com>
2025-10-23 21:56:21 +0200

    added tested cases and small refactoring

--- test/extended-url-mixin.test.ts
+++ test/extended-url-mixin.test.ts
@@ -3,7 +3,11 @@ import { describe, expect, it } from 'vitest'
 import UrlRepr from '../src/classes/url-repr'
 
 import { getTestInstances } from './helpers/get-test-instances'
-
+import {
+    ENCOD_CYRILLIC,
+    ENCOD_SURROGATE,
+    ENCOD_MALFORMED_SURROGATE
+} from './helpers/encoding-test-values'
 
 
 describe("#ExtendedUrlMixin", () => {
@@ -20,13 +24,28 @@ describe("#ExtendedUrlMixin", () => {
             {   arg: ['my', 'PATH'],
                 result: '/my/PATH/'
             },
-            // test escaping
-            {   arg: ['шеллы'],
+            // encoding : non-ascii characters
+            {   arg: [ENCOD_CYRILLIC], // encoding 
                 result: '/%D1%88%D0%B5%D0%BB%D0%BB%D1%8B/'
             },
-            // test escaping
+            // encoding : surrogates (valid and invalid)
+            {   arg: [ENCOD_SURROGATE, ENCOD_MALFORMED_SURROGATE],
+                result: '/%F0%90%8F%BF/%EF%BF%BD/'
+            },
+            // encoding : surrogates (valid and invalid) mixed with ascii
+            {   arg: [ENCOD_SURROGATE + '-text', ENCOD_MALFORMED_SURROGATE + '-other-text'],
+                result: '/%F0%90%8F%BF-text/%EF%BF%BD-other-text/'
+            },
+            {   arg: ['text-' + ENCOD_SURROGATE, 'other-text-' + ENCOD_MALFORMED_SURROGATE],
+                result: '/text-%F0%90%8F%BF/other-text-%EF%BF%BD/'
+            },
+            // encoding special chars
             {   arg: [';,/?:@&=+$"`# '],
                 result: '/%3B%2C%2F%3F%3A%40%26%3D%2B%24%22%60%23%20/'
+            },
+            // encoding special chars with surrogate
+            {   arg: [ENCOD_SURROGATE + ';,/?:@&=+$"`# '+ENCOD_MALFORMED_SURROGATE],
+                result: '/%F0%90%8F%BF%3B%2C%2F%3F%3A%40%26%3D%2B%24%22%60%23%20%EF%BF%BD/'
             },
             // non escaped chars
             {   arg: ["-_.!~*'()"],
--- test/xurl.test.ts
+++ test/xurl.test.ts
@@ -5,22 +5,33 @@ import { BrokenUrlRestrictionError } from '../src/classes/errors'
 import RelativeUrl from '../src/classes/relative-url'
 
 import { instancesAreEquivalent } from './helpers/instance-comparaison'
-import { getUrlTransformations } from './helpers/get-url-transformations'
+import {
+    ENCOD_CYRILLIC,
+    ENCOD_SURROGATE,
+    ENCOD_MALFORMED_SURROGATE
+} from './helpers/encoding-test-values'
+
 
 describe("#XUrl", () => {
     
     it('"instance initialisation" : works with valid values', () => {
         // (also checks if credentials restriction successfully filters credentials at creation time)
-        const testUrlData = {
-            url: new URL('https://user:password@sub.domain.com:8000/test/?query=fish&filter=yellow'),
-            urlNoCreds: new URL('https://sub.domain.com:8000/test/?query=fish&filter=yellow'),
-            
-            xUrl: new XUrl('https://user:password@sub.domain.com:8000/test/?query=fish&filter=yellow'),
-            relativeUrl: new RelativeUrl('/test/?query=fish&filter=yellow'),
 
-            full: 'https://user:password@sub.domain.com:8000/test/?query=fish&filter=yellow',
-            base: 'https://user:password@sub.domain.com:8000',
-            tail: '/test/?query=fish&filter=yellow'
+        const urlBase = 'https://user:password@sub.domain.com:8000'
+        const urlBaseNoCreds = 'https://sub.domain.com:8000'
+        // шеллы need to be encoded and '\uDFFF' will cause an error if given to encodeURI
+        const urlTail = `/test/${ENCOD_CYRILLIC}/${ENCOD_SURROGATE}/${ENCOD_MALFORMED_SURROGATE}/?query=fish&filter=yellow`
+
+        const testUrlData = {
+            url: new URL(urlTail, urlBase),
+            urlNoCreds: new URL(urlTail, urlBaseNoCreds),
+            
+            xUrl: new XUrl(urlTail, urlBase),
+            relativeUrl: new RelativeUrl(urlTail),
+
+            full: urlBase + urlTail,
+            base: urlBase,
+            tail: urlTail
         }
 
         const restrictions: (Partial<XUrl.UrlRestrictions_T>|undefined)[] = [
@@ -65,14 +76,17 @@ describe("#XUrl", () => {
     }) 
 
     it('"instance initialisation" : fails with TypeError for invalid urls', () => {
-
         const invalidConstructorArgs: [any, any][] = [
             // invalid urls with no base
             ['justastring', undefined],
             ['http://in valid.com', undefined],
             ['://missing-protocol.com', undefined],
             ['http//missing-colon.com', undefined],
+            ['http://domain.com:aaa', undefined],
             ['http://', undefined],
+            [`protocol-${ENCOD_CYRILLIC}:me@myhouse.tree`, undefined],
+            [`protocol-${ENCOD_SURROGATE}:me@myhouse.tree`, undefined],
+            [`protocol-${ENCOD_MALFORMED_SURROGATE}:me@myhouse.tree`, undefined],
             ['', undefined],
             [null, undefined],
             [undefined, undefined],
@@ -85,7 +99,11 @@ describe("#XUrl", () => {
             ['/', 'http://in valid.com'],
             ['/', '://missing-protocol.com'],
             ['/', 'http//missing-colon.com'],
+            ['/', 'http://domain.com:aaa'],
             ['/', 'http://'],
+            ['/', `protocol-${ENCOD_CYRILLIC}:me@myhouse.tree`],
+            ['/', `protocol-${ENCOD_SURROGATE}:me@myhouse.tree`],
+            ['/', `protocol-${ENCOD_MALFORMED_SURROGATE}:me@myhouse.tree`],
             ['/', ''],
             ['/', 12345],
             ['/', {}],
@@ -207,7 +225,71 @@ describe("#XUrl", () => {
             'tel:+1234567890',
         ]
 
-        const transformations = getUrlTransformations('ABSOLUTE')
+        const transformations = [
+            (url: URL) => url.protocol = 'http:',
+            (url: URL) => url.protocol = 'ftp', // without ':'
+            (url: URL) => url.protocol = `protocol-${ENCOD_CYRILLIC}:`,
+            (url: URL) => url.protocol = `protocol-${ENCOD_SURROGATE}:`,
+            (url: URL) => url.protocol = `protocol-${ENCOD_MALFORMED_SURROGATE}:`,
+
+            (url: URL) => url.username = 'jo:hn',
+            (url: URL) => url.username = `user-${ENCOD_CYRILLIC}`,
+            (url: URL) => url.username = `user-${ENCOD_SURROGATE}`,
+            (url: URL) => url.username = `user-${ENCOD_MALFORMED_SURROGATE}`,
+            
+            (url: URL) => url.password = 'doe@doe',
+            (url: URL) => url.password = `pass-${ENCOD_CYRILLIC}`,
+            (url: URL) => url.password = `pass-${ENCOD_SURROGATE}`,
+            (url: URL) => url.password = `pass-${ENCOD_MALFORMED_SURROGATE}`,
+            
+            (url: URL) => url.host = 'other.example.com:8032',
+            (url: URL) => url.host = `${ENCOD_CYRILLIC}.example.com:8032`,
+            (url: URL) => url.host = `${ENCOD_SURROGATE}.example.com:8032`,
+            (url: URL) => url.host = `${ENCOD_MALFORMED_SURROGATE}.example.com:8032`,
+
+            (url: URL) => url.hostname = 'something.com',
+            (url: URL) => url.hostname = `something-${ENCOD_CYRILLIC}.com:8032`,
+            (url: URL) => url.hostname = `something-${ENCOD_SURROGATE}.com:8032`,
+            (url: URL) => url.hostname = `something-${ENCOD_MALFORMED_SURROGATE}.com:8032`,
+            
+            (url: URL) => url.port = '8080',
+            (url: URL) => url.port = 'aaa',
+            (url: URL) => url.port = ENCOD_CYRILLIC,
+            (url: URL) => url.port = ENCOD_SURROGATE,
+            (url: URL) => url.port = ENCOD_MALFORMED_SURROGATE,
+            
+            (url: URL) => url.pathname = '/newpath/',
+            (url: URL) => url.pathname = 'otherpath', // without '/'
+            (url: URL) => url.pathname = `/path/${ENCOD_CYRILLIC}/`,
+            (url: URL) => url.pathname = `/path/${ENCOD_SURROGATE}/`,
+            (url: URL) => url.pathname = `/path/${ENCOD_MALFORMED_SURROGATE}/`,
+            
+            (url: URL) => url.search = '?newquery=1',
+            (url: URL) => url.search = 'otherquery=2', // without '?'
+            (url: URL) => url.search = `?search=${ENCOD_CYRILLIC}`,
+            (url: URL) => url.search = `?search=${ENCOD_SURROGATE}`,
+            (url: URL) => url.search = `?search=${ENCOD_MALFORMED_SURROGATE}`,
+            
+            (url: URL) => url.hash = '#section2',
+            (url: URL) => url.hash = 'section3', // without '#'
+            (url: URL) => url.hash = `#${ENCOD_CYRILLIC}`,
+            (url: URL) => url.hash = `#${ENCOD_SURROGATE}`,
+            (url: URL) => url.hash = `#${ENCOD_MALFORMED_SURROGATE}`,
+
+            (url: URL) => url.href = 'http://www.somewhere.zz/some/path?hello=world#cake',
+            (url: URL) => url.href = 'http://www.somewhere.zz', // no tail
+            (url: URL) => url.href = `http://www.somewhere.zz/${ENCOD_CYRILLIC}/path?hello=world#cake`,
+            (url: URL) => url.href = `http://www.somewhere.zz/${ENCOD_SURROGATE}/path?hello=world#cake`,
+            (url: URL) => url.href = `http://www.somewhere.zz/${ENCOD_MALFORMED_SURROGATE}/path?hello=world#cake`,
+            (url: URL) => url.href = `http://www.somewhere.zz/path?hello=${ENCOD_CYRILLIC}#cake`,
+            (url: URL) => url.href = `http://www.somewhere.zz/path?hello=${ENCOD_SURROGATE}#cake`,
+            (url: URL) => url.href = `http://www.somewhere.zz/path?hello=${ENCOD_MALFORMED_SURROGATE}#cake`,
+            (url: URL) => url.href = `http://www.somewhere.zz/path?hello=world#${ENCOD_CYRILLIC}`,
+            (url: URL) => url.href = `http://www.somewhere.zz/path?hello=world#${ENCOD_SURROGATE}`,
+            (url: URL) => url.href = `http://www.somewhere.zz/path?hello=world#${ENCOD_MALFORMED_SURROGATE}`,
+            (url: URL) => url.href = 'tel:+9876543210', // with tel: protocol
+            (url: URL) => url.href = 'mailto:me@myhouse.tree', // with mailto: protocol
+        ]
 
         for (const urlString of urlStrings) {
 
